888c89fca796ee9dd8904e9497588a0c9f3044d1
bilgramiraza <bilgramiraza@yahoo.com>
2022-09-28 19:44:17 +0530

    Implemented Task Items to list all tasks

--- frontend/src/components/AllTasks.js
+++ frontend/src/components/AllTasks.js
@@ -1,17 +1,27 @@
 import { useGetTasksQuery } from '../store/taskSlice';
+import TaskItem from './TaskItem';
 
 const AllTasks = () => {
   const { data: tasks, isLoading, isSuccess, isError, error } = useGetTasksQuery();
 
   let content;
+  console.log(tasks);
   if (isLoading) {
     content = <p>Loading...</p>;
   } else if (isSuccess) {
-    content = <p>{tasks}</p>;
+    content = tasks.ids.map((taskId) => (
+      <li>
+        <TaskItem key={taskId} taskId={taskId} />
+      </li>
+    ));
   } else if (isError) {
     content = <p>{error}</p>;
   }
-  return <div>{content}</div>;
+  return (
+    <section>
+      <ul>{content}</ul>
+    </section>
+  );
 };
 
 export default AllTasks;
